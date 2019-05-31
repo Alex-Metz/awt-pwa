734f1bcbb29bfa0d2923eca18eecc74985b7ef29
Alexander Metzler <alexander.metzler@campus.tu-berlin.de>
2019-05-31 12:01:00 +0200

    Updates

---
+++ precache-manifest.793aae9704f3a9df044ad3fd745e0b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8514b5ba53f9f906f7fb402dedda1930",
+    "url": "/awt-pwa/index.html"
+  },
+  {
+    "revision": "b3fd2c5c3f9b8bccb0fd",
+    "url": "/awt-pwa/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b3fd2c5c3f9b8bccb0fd",
+    "url": "/awt-pwa/static/js/2.716291fc.chunk.js"
+  },
+  {
+    "revision": "9b2e4e086353c67424a8",
+    "url": "/awt-pwa/static/js/main.82dbd030.chunk.js"
+  },
+  {
+    "revision": "d3e6c92ec33f19987f67",
+    "url": "/awt-pwa/static/js/runtime~main.a5205106.js"
+  }
+]);
